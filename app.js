adff2fc22fa3a4e616ffa3dcf75e3a1ae82c139a
rsampaoli <106210672+rsampaoli@users.noreply.github.com>
2022-08-26 23:39:39 -0300

    Creación de Login.

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(3030, () => { console.log("server iniciado") });
-app.get("/", (res, req) => { req.sendFile(path.join(__dirname, "/views/home.html")) });
-//app.get("/", (res, req) => { req.sendFile(path.join(__dirname, "/views/register.html")) });
+//app.get("/", (res, req) => { req.sendFile(path.join(__dirname, "/views/home.html")) });
+app.get("/", (res, req) => { req.sendFile(path.join(__dirname, "/views/register.html")) });
