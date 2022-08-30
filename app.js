deb38356537a1076c633e34aa020de0c6db4f42e
rsampaoli <106210672+rsampaoli@users.noreply.github.com>
2022-08-29 21:02:30 -0300

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(3030, () => { console.log("server iniciado") });
+app.listen(process.env.PORT || 3030, () => { console.log("server iniciado") });
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
